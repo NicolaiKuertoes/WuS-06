4905f26efdba8423df925d4e6813fbe7a0a42adb
Nicolai Kuertoes <github@kuertoes.de>
2021-02-18 16:13:38 +0100

    100%

--- WuS-06_Gummibaerchen-c.py
+++ WuS-06_Gummibaerchen-c.py
@@ -1,3 +1,4 @@
+# calculate chi^2 with Σ((o-e)²)/e, where o is the observed value and e is the expected value
 chi = sum([((gummibaerchen.Beobachtet[i] - gummibaerchen.Erwartet[i])**2) / gummibaerchen.Erwartet[i] for i in range(len(gummibaerchen.Sorte))])
 
 chi

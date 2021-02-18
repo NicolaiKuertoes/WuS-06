32bed4a860a9d45e26724c95cd4a3c4b7b708ab7
Nicolai Kuertoes <github@kuertoes.de>
2021-02-18 17:20:22 +0100

    0%

---
+++ WuS-06_Exakter-Test-nach-Fischer.py
@@ -0,0 +1,25 @@
+import math
+
+def fisher_exakt(vierfeldertafel) -> float:
+    '''
+    Führe den zweiseitigen Fisher-Test für eine Vierfeldertafel aus
+    
+    Arguments:
+        vierfeldertafel -- Die Vierfeldertafel
+    Returns:
+        p_Wert          -- Der p-Wert 
+    '''
+    p_Wert = 0
+    # YOUR CODE HERE
+    raise NotImplementedError()
+    
+    return p_Wert
+
+# YOUR CODE HERE
+raise NotImplementedError()
+
+    
+vierfeldertafel = [[4, 1], 
+                   [2, 2]]
+
+fisher_exakt(vierfeldertafel), stats.fisher_exact(vierfeldertafel)

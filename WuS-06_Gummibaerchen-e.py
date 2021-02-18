c1bb8d7b37784ca792c5e5550d81cb9db086e01c
Nicolai Kuertoes <github@kuertoes.de>
2021-02-18 16:44:08 +0100

    100%

--- WuS-06_Gummibaerchen-e.py
+++ WuS-06_Gummibaerchen-e.py
@@ -2,17 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chi2
 
-# YOUR CODE HERE
+# generate evenly spaced numbers over a specified interval
 x = np.linspace(0, 20 ,100)
+
+# probability density function evaluated at x
 y = chi2.pdf(x, 5)
 
+# plot density function
 plt.plot(x, y, label='Dichtefunktion $\chi^2$')
-plt.vlines([chi2.ppf(q=untereGrenze, df=5), chi2.ppf(q=obereGrenze, df=5)], [0, 0], [0.16, 0.16], colors='red', label='Grenzen des Annahmebereichs')
-plt.vlines([13.14], [0], [0.16], colors='green', label='Prüfgröße')
+
+# plot vertical lines at "untereGrenze" and "obereGrenze"
+plt.vlines([untereGrenze, obereGrenze], [0, 0], [0.16, 0.16], colors='red', label='Grenzen des Annahmebereichs')
+
+# plot vertical line for chi^2-value
+plt.vlines(chi, [0], [0.16], colors='green', label='Prüfgröße')
+
+# set the label for x-axis
 plt.xlabel('x')
+
+# set the label for y-axis
 plt.ylabel('y')
+
+# set a title for the axis
 plt.title('$\chi^2$ Verteilung mit Annahmebereich')
+
+# configure the grid lines
 plt.grid()
+
+# place a legend on the axes.
 plt.legend(loc='best')
+
+# display all figures.
 plt.show()
-# TEXT für vertikale Linien (oben, unten)

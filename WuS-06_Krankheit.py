cb2ce2bd26a1fe94c3de829d7118cd5b4d59655b
Nicolai Kuertoes <github@kuertoes.de>
2021-02-18 08:28:23 +0100

    FIXED

--- WuS-06_Krankheit.py
+++ WuS-06_Krankheit.py
@@ -47,12 +47,12 @@ def kontingenztafel_test(kontingenztafel: [[float]]) -> Tuple[float, float, floa
     # empty array for expected values
     exp = []
     
-    # calculate expected values and add them to exp ------------------------------ TODO: FIX BUG
+    # calculate expected values and add them to exp
     for i in range(len(kontingenztafel)):
-        for k in range(len(kontingenztafel)):
+        for k in range(len(kontingenztafel[0])):
             exp.append((obs_sum_rows[i] * obs_sum_cols[k]) / obs_total)
             
-    # print(exp) # --------------------------------------------------------------- Test
+    print(exp)
             
     # convert exp to numpy array for convenience
     exp = np.array(exp)
@@ -65,7 +65,7 @@ def kontingenztafel_test(kontingenztafel: [[float]]) -> Tuple[float, float, floa
 
 
 # Aufruf mit Beispiel aus der Vorlesung
-#pruefgroesse, freiheitsgrade, p_Wert = kontingenztafel_test([[17, 38], [18, 7]])
+pruefgroesse, freiheitsgrade, p_Wert = kontingenztafel_test([[17, 38], [18, 7]])
 # Ausgabe der Werte
 print(pruefgroesse, freiheitsgrade, p_Wert)
 
